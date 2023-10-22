ad411d2aaeb67f317bbeeb2f3f74cbbdff5c305c
Joe McBride <255007+joemcbride@users.noreply.github.com>
2023-10-22 15:23:07 -0700

    Keymap

--- keyboards/controllerworks/mini42/keymaps/joemcbride/keymap.c
+++ keyboards/controllerworks/mini42/keymaps/joemcbride/keymap.c
@@ -29,11 +29,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,   KC_L,    KC_U,   KC_Y,   KC_SCLN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_ESC, KC_A,  LCTL_T(KC_R), LALT_T(KC_S), LGUI_T(KC_T),  KC_G,             KC_M,  RGUI_T(KC_N),RALT_T(KC_E),RCTL_T(KC_I), KC_O,  KC_QUOT,
+      QK_GESC, KC_A,  LCTL_T(KC_R), LALT_T(KC_S), LGUI_T(KC_T),  KC_G,             KC_M,  RGUI_T(KC_N),RALT_T(KC_E),RCTL_T(KC_I), KC_O,  KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_D,                        KC_K,    KC_H,  KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_ENT),
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_D,                        KC_K,    KC_H,  KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_GRV),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(2),  KC_SPC,     KC_ENT,   MO(3), KC_RALT
+                                         KC_LGUI, MO(_NUM), KC_SPC,     KC_ENT, MO(_SYM), KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -41,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,   KC_L,    KC_U,   KC_Y,   KC_SCLN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_ESC,  KC_A, LGUI_T(KC_R), LALT_T(KC_S), LCTL_T(KC_T),KC_G,              KC_M, RCTL_T(KC_N),RALT_T(KC_E,   KC_I,    KC_O,  KC_QUOT,
+     QK_GESC, KC_A, LGUI_T(KC_R),LALT_T(KC_S),LCTL_T(KC_T),KC_G,                KC_M, RCTL_T(KC_N),RALT_T(KC_E), RGUI_T(KC_I), KC_O,  KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_D,                        KC_K,    KC_H,  KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_ENT),
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_D,                        KC_K,    KC_H,  KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_GRV),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL,   MO(2),  KC_SPC,     KC_ENT,   MO(3), KC_RALT
+                                          KC_LCTL, MO(_NUM), KC_SPC,    KC_ENT, MO(_SYM), KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -53,29 +53,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP,                      KC_LEFT,  KC_UP,  KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX,
+      QK_GESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP,                      KC_LEFT,  KC_DOWN,  KC_UP, KC_RIGHT, XXXXXXX, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, KC_PGDN,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_LSFT, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, KC_PGDN,                      KC_HOME, XXXXXXX, XXXXXXX, KC_END, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_ESC,     KC_ENT,   MO(4), KC_RALT
+                                          KC_LGUI, _______,  KC_ESC,    KC_ENT, MO(_ADJ), KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_SYM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+      KC_LCTL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+      KC_LSFT,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(4),  KC_ESC,     KC_DEL, _______, KC_RALT
+                                         KC_LGUI, MO(_ADJ), KC_ESC,     KC_DEL, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_ADJ] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     TG(_WIN), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_NUM,
+      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     TG(_WIN), KC_MUTE, KC_VOLU, KC_VOLD, XXXXXXX,  KC_NUM,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
--- keyboards/controllerworks/mini42/mini42.c
+++ keyboards/controllerworks/mini42/mini42.c
@@ -41,12 +41,12 @@ oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
         return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
     }
 
-    return rotation;
+    return OLED_ROTATION_90;
 }
 
 bool render_status(void) {
     // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
+    // oled_write_P(PSTR("Layer: "), false);
 
     switch (get_highest_layer(layer_state)) {
         case _MAC:
@@ -56,24 +56,24 @@ bool render_status(void) {
             oled_write_P(PSTR("WIN\n"), false);
             break;
         case _NUM:
-            oled_write_P(PSTR("NUMBRS\n"), false);
+            oled_write_P(PSTR("NUM\n"), false);
             break;
         case _SYM:
             oled_write_P(PSTR("SYM\n"), false);
             break;
         case _ADJ:
-            oled_write_P(PSTR("ADJUST\n"), false);
+            oled_write_P(PSTR("ADJ\n"), false);
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
+            oled_write_ln_P(PSTR("Undef"), false);
     }
 
     // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    oled_write_P(led_state.num_lock ? PSTR("NUM\n") : PSTR("   \n"), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP\n") : PSTR("   \n"), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR\n") : PSTR("   \n"), false);
 
     return false;
 }
